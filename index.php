879369fb0bd233ed963dd1b41a8104ed115a3f2e
unknown <chaconproducciones@gmail.com>
2018-01-06 11:39:50 -0400

    Version Original

---
+++ index.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="">
+
+    <title>Detektei-Web</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/generic_detektei.css" rel="stylesheet">
+</head>
+<body>
+    <div id="app" class="container">
+              <div class="row">
+
+
+                
+                <div id="web" class="col-lg-3 col-md-3 col-sm-12">
+                  <div class="row">
+                      <img class="img-fluid" src="img/logo_neu_web.png" />
+                  </div>
+                </div>
+                <div class="col-lg-9 col-md-9 col-sm-12">
+                  <div class="row">
+                      <div class="col-lg-6 col-md-6 col-sm-6">
+                        <div id="web" class="pull-left">
+                          Persönliche Beratung: 07031 / 498465 (Mo. bis Fr. von 8 bis 20 Uhr)
+                        </div>
+                        <div id="mobile" class="pull-left">
+                          07031 / 498465 (Mo. bis Fr. von 8 bis 20 Uhr)
+                        </div>
+                      </div>
+                      <div class="col-lg-6 col-md-6 col-sm-6">
+                        <div class="wine-color-bg font-white-bold pull-right" style="padding-right:10px;">
+                          <img src="img/Pikto_Kontakt.png" width="auto" height="auto" style="padding-right:5px;">
+                          CALL BACK - Service - Wir rufen Sie an!
+                        </div>
+                      </div>
+                  </div>
+                </div>
+                <div id="mobile" class="col-lg-3 col-md-3 col-sm-12">
+                  <div class="row">
+                      <img class="img-fluid" src="img/logo_neu_web.png" />
+                  </div>
+                </div>
+                
+              </div>
+              <div class="sidebar col-lg-3 col-md-3">
+                      <div class="formulario">
+                        <input type="text"/>
+
+                      </div>
+              
+
+              
+              </div>
+    </div>
+    
+</body>
+</html>
