dbd651f5463aaf887624a5e1b2c045777e70030a
Neymar Contreras <jcontreras@e4gslab.com>
2018-01-06 15:14:28 -0400

    Ajustes merge

--- index.php
+++ index.php
@@ -30,6 +30,26 @@
                   <img id='detektei' src='img/logo_neu_web.png'/>
                 </div>
             </figure>
+            <p>Seit über 25 Jahren europaweit tätig.</p>
+            <div class="row">
+              <div class="content-sidebar">
+                <div class="sidebar">
+                  <form>
+                      <p class="text-sidebar" style="font-size:24px; line-height:100%;">Nehmen Sie mit uns Kontakt auf. Wir rufen zurück!.</p>
+                      <input class="input-formulario" type="text"/>
+                      <p class="text-sidebar">Name</p>
+                      <input class="input-formulario" type="text"/>
+                      <p class="text-sidebar">Name</p>
+                      <div>
+                        <input class="input-formulario" type="text"/>
+
+                          <p class="text-sidebar">Name</p>
+                          <div id="circulo">Senden</div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+            </div>
           </div>
           <div  id="web" class="col-lg-9 col-md-9 col-sm-12">
             <!-- Init Content Web -->
@@ -42,7 +62,6 @@
                 <div class="container-detektei">
                   <img id='detektei' src='img/logo_neu_web.png'/>
                 </div>
-<<<<<<< Updated upstream
             </figure>
           </div>
           <div id="mobile" class="col-sm-12">
@@ -51,42 +70,6 @@
           </div>
           <!-- End Mobile -->
         </div>
-=======
-
-              </div>
-              <p>Seit über 25 Jahren europaweit tätig.</p>
-              
-              <div class="row">
-                
-                 
-               
-               
-                 
-             
-                
-              <div class="content-sidebar col-md-3">
-                
-                <div class="sidebar">
-                  <form>
-                  <p class="text-sidebar" style="font-size:24px; line-height:100%;">Nehmen Sie mit uns Kontakt auf. Wir rufen zurück!.</p>
-
-                    
-                      
-                    <input class="input-formulario" type="text"/>
-                      <p class="text-sidebar">Name</p>
-                      <input class="input-formulario" type="text"/>
-                      <p class="text-sidebar">Name</p>
-                      <div>
-                      <input class="input-formulario" type="text"/>
-                      
-                        <p class="text-sidebar">Name</p>
-                        <div id="circulo">Senden</div>
-                      </div>
-                </form>
-                </div>
-                </div>
-              </div>  
->>>>>>> Stashed changes
         <?php
             @include("directorio/commons/footer.php");
          ?>
