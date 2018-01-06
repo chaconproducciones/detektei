5f1b93d2b410e2efa7f33adf3d2395e315bc26ad
Neymar Contreras <jcontreras@e4gslab.com>
2018-01-06 18:32:07 -0400

    Ajustes del Merge

--- index.php
+++ index.php
@@ -19,98 +19,61 @@
 </head>
 <body>
     <div id="app" class="container">
-              <div class="row">
-                <div id="web" class="col-lg-3 col-md-3 col-sm-12">
-                  <figure class="detektei">
-                      <div class="container-detektei">
-                        <img id='detektei' src='img/logo_neu_web.png'/>
-                      </div>
-                  </figure>
+        <div class="row">
+          <!-- Init Header -->
+          <?php
+              @include("directorio/commons/header.php");
+           ?>
+          <!-- End Header -->
+          <!-- Init Web -->
+          <div id="web" class="col-lg-3 col-md-3 col-sm-12">
+            <figure class="detektei">
+                <div class="container-detektei">
+                  <img id='detektei' src='img/logo_neu_web.png'/>
                 </div>
-                <div class="col-lg-9 col-md-9 col-sm-12">
-                  <div class="row">
-                      <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div id="web" class="pull-left">
-                          Persönliche Beratung: 07031 / 498465 (Mo. bis Fr. von 8 bis 20 Uhr)
-                        </div>
-                        <div id="mobile" class="pull-left">
-                          07031 / 498465 (Mo. bis Fr. von 8 bis 20 Uhr)
-                        </div>
-                      </div>
-                      <div class="col-lg-6 col-md-6 col-sm-6">
-                        <div class="wine-color-bg font-white-bold pull-right" style="padding-right:10px;">
-                          <img src="img/Pikto_Kontakt.png" width="auto" height="auto" style="padding-right:5px;">
-                          CALL BACK - Service - Wir rufen Sie an!
-                        </div>
-                      </div>
-                  </div>
-                </div>
-                <div id="mobile" class="col-sm-12">
-                  <figure class="detektei">
-                      <div class="container-detektei">
-                        <img id='detektei' src='img/logo_neu_web.png'/>
-                      </div>
-                  </figure>
-                </div>
-
-              </div>
-              <div class="alinear-derecha col-md-3">
-                Seit über 25 Jahren europaweit tätig.
+            </figure>
+            <div class="alinear-derecha">
+              Seit über 25 Jahren europaweit tätig.
               <br/>
               Mitglied im Berufsverband BID.
-            </div> 
-              <div class="row">
-                
-                  
-               
-               
-                 
-             
-                
-              <div class="content-sidebar col-md-3">
-              
-             
+            </div>
+            <div class="row">
+              <div class="content-sidebar">
                 <div class="sidebar">
                   <form>
-                  <p class="text-sidebar" style="font-size:24px; line-height:100%;">Nehmen Sie mit uns Kontakt auf. Wir rufen zurück!.</p>
-
-                    
-                      
+                    <p class="tittle-sidebar">Nehmen Sie mit uns Kontakt auf. Wir rufen zurück!.</p>
                     <input class="input-formulario" type="text"/>
-                      <p class="text-sidebar">Name</p>
-                      <input class="input-formulario" type="text"/>
-                      <p class="text-sidebar">E-Mail</p>
-                      <div>
+                    <p class="text-sidebar">Name</p>
+                    <input class="input-formulario" type="text"/>
+                    <p class="text-sidebar">E-Mail</p>
+                    <div>
                         <input class="input-formulario" type="text"/>
                         <p class="text-sidebar">Nachricht</p>
                         <table>
-                        <tr>
-                        <div class="col-med-3">
-                        <td>  
-                          <input class="input-formulario2" type="text"/>
-                          <p class="text-sidebar">Zeitfenster für Rückruf</p>
-                        </td>
-                        <td>
-                          <div id="circulo">Senden</div>
-                        </td>
-                        </div>
-                        </tr>
-                      </table>
-                      </div>
-                      
-                </form>
-               
-                
+                          <tr>
+                          <div class=" col-lg-3 col-md-3 col-sm-12">
+                          <td>
+                            <input class="input-formulario2" type="text"/>
+                            <p class="text-sidebar">Zeitfenster für Rückruf</p>
+                          </td>
+                          <td>
+                            <div id="circulo">Senden</div>
+                          </td>
+                          </div>
+                          </tr>
+                        </table>
+                    </div>
+                  </form>
                 </div>
-                <img class="puzzle" src='img/puzzle.jpg'/> 
+                <img class="puzzle" src='img/puzzle.jpg'/>
                 <div class="tablasidebar">
                   <table>
                     <tr>
-                    <p class="text-sidebar1">Die Detektei Larsen: Verbände / Zertifkate</p>
+                    <p class="text-sidebar-bottom">Die Detektei Larsen: Verbände / Zertifkate</p>
                     </tr>
                     <tr>
                       <td>
-                      <img class="banner" src='img/bid-Logo Zertifizierung_animiert.png'/> 
+                      <img class="banner" src='img/bid-Logo Zertifizierung_animiert.png'/>
 
                       </td>
                       <td><img class="banner" src='img/IKD_Logo.png'/> </td>
@@ -123,6 +86,21 @@
                     </tr>
                   </table>
                 </div>
+            </div>
+          </div>
+          </div>
+          </div>
+          <div  id="web" class="col-lg-9 col-md-9 col-sm-12">
+            <!-- Init Content Web -->
+            <!-- End Content Web -->
+          </div>
+          <!-- End Web -->
+          <!-- Init Mobile -->
+          <div id="mobile" class="col-sm-12">
+            <figure class="detektei">
+                <div class="container-detektei">
+                  <img id='detektei' src='img/logo_neu_web.png'/>
+                </div>
             </figure>
           </div>
           <div id="mobile" class="col-sm-12">
@@ -130,14 +108,13 @@
             <!-- End Content Mobile -->
           </div>
           <!-- End Mobile -->
-        </div>
-        <div class="row wine-color-bg">
-          <?php
-              @include("directorio/commons/footer.php");
-           ?>
-        </div>
-              </div>
-              </div>  
-    </div>
+          <!-- Init Footer -->
+          <div class="row wine-color-bg">
+            <?php
+                @include("directorio/commons/footer.php");
+             ?>
+          </div>
+          <!-- End Footer -->
+  </div>
 </body>
 </html>
