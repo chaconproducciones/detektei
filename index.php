1bc0bc87de77f2ab48419d92f687625f51b42994
cesar <chaconproducciones@gmail.com>
2018-01-06 15:10:08 -0400

    Sidebar1

--- index.php
+++ index.php
@@ -42,6 +42,7 @@
                 <div class="container-detektei">
                   <img id='detektei' src='img/logo_neu_web.png'/>
                 </div>
+<<<<<<< Updated upstream
             </figure>
           </div>
           <div id="mobile" class="col-sm-12">
@@ -50,6 +51,42 @@
           </div>
           <!-- End Mobile -->
         </div>
+=======
+
+              </div>
+              <p>Seit über 25 Jahren europaweit tätig.</p>
+              
+              <div class="row">
+                
+                 
+               
+               
+                 
+             
+                
+              <div class="content-sidebar col-md-3">
+                
+                <div class="sidebar">
+                  <form>
+                  <p class="text-sidebar" style="font-size:24px; line-height:100%;">Nehmen Sie mit uns Kontakt auf. Wir rufen zurück!.</p>
+
+                    
+                      
+                    <input class="input-formulario" type="text"/>
+                      <p class="text-sidebar">Name</p>
+                      <input class="input-formulario" type="text"/>
+                      <p class="text-sidebar">Name</p>
+                      <div>
+                      <input class="input-formulario" type="text"/>
+                      
+                        <p class="text-sidebar">Name</p>
+                        <div id="circulo">Senden</div>
+                      </div>
+                </form>
+                </div>
+                </div>
+              </div>  
+>>>>>>> Stashed changes
         <?php
             @include("directorio/commons/footer.php");
          ?>
