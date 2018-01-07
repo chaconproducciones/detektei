c2ea6e9e85f3214f8ed2430f490f421d2d8a1463
cesar <chaconproducciones@gmail.com>
2018-01-07 02:39:23 -0400

    ActualizacionPrivat1

--- directorio/commons/footer.php
+++ directorio/commons/footer.php
@@ -3,7 +3,7 @@
   <img src="img/Pikto_Home.png" width="auto" height="auto">
   <img src="img/Pikto_Kontakt.png" width="auto" height="auto">
   <img src="img/Pikto_Ort.png" width="auto" height="auto">
-  <img src="img/Pikto_Info.png" width="auto" height="auto">
+  <a href="directorio/privat/privat.php"><img src="img/Pikto_Info.png" width="auto" height="auto"></a>
 </div>
 <div id="web" class="col-lg-9 col-md-9 col-sm-12  wine-color-bg font-footer center-block">
       Lothar Komorowski - Geprüfter Detektiv | Brunnenstr. 31/1, 71032 Böblingen | T.: 07031 / 498465 | E.: <a href="mailto:info@detektei-larsen.de" class="font-footer">info@detektei-larsen.de</a> | Callback-Service | Impressum
@@ -15,7 +15,7 @@
         <img src="img/Pikto_Home.png" width="auto" height="auto">
         <img src="img/Pikto_Info.png" width="auto" height="auto">
         <img src="img/Pikto_Kontakt.png" width="auto" height="auto">
-        <img src="img/Pikto_Ort.png" width="auto" height="auto">
+        <a href="directorio/privat/privat.php"><img src="img/Pikto_Ort.png" width="auto" height="auto"></a>
     </div>
 </div>
 <!-- End section mobile -->
